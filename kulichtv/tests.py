daf2b852ac3b1b64c2f426787552bef4c4a6bf0a
alex-klim <goldencrane@ukr.net>
2017-11-08 13:35:50 +0200

    add kulichtv app tests

--- kulichtv/tests.py
+++ kulichtv/tests.py
@@ -1,3 +1,29 @@
+from collections import Counter
 from django.test import TestCase
+from .models import *
+from django.urls import reverse
 
-# Create your tests here.
+
+class ViewsTest(TestCase):
+    def setUp(self):
+       Game.objects.create(title='gametitle', descr='gamedescr')
+       Game.objects.create(title='gametitle1', descr='gamedescr1')
+       Game.objects.create(title='gametitle2', descr='gamedescr2')
+
+    def testGamesView(self):
+        status = self.client.get(reverse('index')).status_code
+        self.assertEqual(status, 200)
+        self.assertTemplateUsed('kulichtv/index.html')
+
+    def testCommunitiesView(self):
+        status = self.client.get(reverse('index')).status_code
+        self.assertEqual(status, 200)
+        self.assertTemplateUsed('kulichtv/communities.html')
+
+    def testGameAdd(self):
+        gameobj = Game.objects.get(title='gametitle')
+        self.assertIsNone(gameobj.pic._file)
+
+    def testGamesCount(self):
+        count = Game.objects.all().count()
+        self.assertEqual(count, 3)
